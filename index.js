2fc7f47a599bb6621bf1617cbc3d0886ccde6c96
lionls <lionel.schockenhoff@hhu.de>
2020-12-13 16:53:52 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,41 +1,40 @@
-'use strict';
-const react = require('react');
+const react = require("react");
 const useEffect = react.useEffect;
 const useState = react.useState;
-const throttle = require('lodash.throttle');
+const throttle = require("lodash.throttle");
 
 module.exports = (offset = 0) => {
-	const [ref, setRef] = useState(null);
-	const [inView, setInView] = useState(false);
-	useEffect(() => {
-		const elementYPos = () => {
-			return (
-				ref.getBoundingClientRect().top +
-				window.pageYOffset -
-				window.innerHeight +
-				offset
-			);
-		};
+  const [ref, setRef] = useState(null);
+  const [inView, setInView] = useState(false);
+  useEffect(() => {
+    const elementYPos = () => {
+      return (
+        ref.getBoundingClientRect().top +
+        window.pageYOffset -
+        window.innerHeight +
+        offset
+      );
+    };
 
-		if (!ref) {
-			return;
-		} 
+    if (!ref) {
+      return;
+    }
 
-		const handleScroll = throttle(() => {
-			if (!ref) {
-				return;
-			}
+    const handleScroll = throttle(() => {
+      if (!ref) {
+        return;
+      }
 
-			if (window.pageYOffset >= elementYPos()) {
-				window.removeEventListener('scroll', handleScroll);
-				setInView(true);
-			}
-		}, 200);
-		window.addEventListener('scroll', handleScroll, {passive: true});
-		handleScroll();
+      if (window.pageYOffset >= elementYPos()) {
+        window.removeEventListener("scroll", handleScroll);
+        setInView(true);
+      }
+    }, 200);
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    handleScroll();
 
-		return () => window.removeEventListener('scroll', handleScroll);
-	}, [ref, offset]);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [ref, offset]);
 
-	return [setRef, inView];
+  return [setRef, inView];
 };
